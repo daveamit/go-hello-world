01a8a24d365533e7429a828e0b86a13692a5b690
Dave Amit <self@daveamit.com>
2018-08-16 13:53:24 +0530

    feat: Added version support

--- main.go
+++ main.go
@@ -10,6 +10,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Following variables will be statically linked at the time of compiling
+
+// GitCommit holds short commit hash of source tree
+var GitCommit string
+
+// GitBranch holds current branch name the code is built off
+var GitBranch string
+
+// GitState shows whether there are uncommitted changes
+var GitState string
+
+// GitSummary holds output of git describe --tags --dirty --always
+var GitSummary string
+
+// BuildDate holds RFC3339 formatted UTC date (build time)
+var BuildDate string
+
+// Version holds contents of ./VERSION file, if exists, or the value passed via the -version option
+var Version string
+
 var port = flag.Int("port", 8000, "port to serve to")
 
 func init() {
@@ -30,6 +50,16 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	fmt.Printf(`
+	GitCommit: %s
+	GitBranch: %s
+	GitState: %s
+	GitSummary: %s
+	BuildDate: %s
+	Version: %s
+	`, GitCommit, GitBranch, GitState, GitSummary, BuildDate, Version)
+
 	// Build the address:port, if nothing was passed we have 8000 at
 	// port and it would result in "127.0.0.1:8000"
 	addr := fmt.Sprintf("127.0.0.1:%d", *port)
